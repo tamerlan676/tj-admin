fd47d09fb09c90aef55780da59ad52945fd8043e
godgiev <godgiev@tjcollection.ru>
2022-03-21 16:45:37 +0300

    start auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,3 +3,21 @@
     <h1>Home</h1>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const user = ref(null)
+    axios.post('http://192.168.200.32:8100/api/v1/auth/login/', {
+      username: 'tamerlan676@gmail.com',
+      password: '12345six'
+    })
+    return {
+      user
+    }
+  }
+}
+</script>
